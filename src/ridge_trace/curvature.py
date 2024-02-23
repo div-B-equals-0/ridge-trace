bd0a9d61c546cdea1fb1cbeaad53004c812ee02e
William Henney <will@henney.org>
2024-02-22 23:24:39 -0600

    Refactor as class and add spatial filtering and thresholding

--- src/ridge_trace/curvature.py
+++ src/ridge_trace/curvature.py
@@ -5,46 +5,126 @@ from astropy.convolution import convolve_fft
 from astropy.convolution.kernels import Gaussian2DKernel
 
 
-def principal_curvature(image, sigma=None):
-    """Calculate principal curvatures and principal directions from an image
+class ImageCurvature:
+    """Calculate principal curvatures and principal directions from an image."""
 
-    Args:
-        image: 2D array
-            Image to calculate curvature from
-        sigma: float
-            Standard deviation of the Gaussian kernel used for smoothing
+    def __init__(self, image, smooth=None, sharp=None, max=None, min=None, bbox=None):
+        """Calculate principal curvatures and principal directions from an image
 
-    Returns:
-        kappa1: 2D array
-            First principal curvature (higher value)
-        kappa2: 2D array
-            Second principal curvature (lower value)
-        dir1: 2D array
-            Principal direction 1
-        dir2: 2D array
-            Principal direction 2
-    """
+        Parameters
+        ----------
+        image : array_like
+            The image to calculate the curvatures from
+        smooth : float, optional
+            The scale of the low-pass filter to apply to the image.
+            This is the sigma (in pixels) of a Gaussian kernel that will be convolved
+            with the image, which will remove any smaller-scale noise
+        sharp : float, optional
+            The scale of the high-pass filter to apply to the image.
+            This is the sigma (in pixels) of a Gaussian kernel that will be convolved
+            with the image before subtracting from the original image, which will
+            remove any smooth larger-scale features
+        max : float, optional
+            The maximum value to threshold the image at. This is useful for removing
+            bright stars or other features that may dominate the curvature calculation.
+            Pixels with values greater than `max` will be set to NaN, so that they will
+            be interpolated over by the smoothing.
+        min : float, optional
+            The minimum value to threshold the image at. This is useful for removing
+            negative instrumental artefacts or noisy regions that may interfere with
+            the curvature calculation. Pixels with values greater than `min` will be
+            set to NaN, so that they will be interpolated over by the smoothing.
+        bbox : x1 y1 x2 y2, optional
+            The bounding box of the region to calculate the curvatures in. If not
+            provided, the entire image will be used. An additional margin is added
+            around the bounding box for the spatial filtering and derivative
+            determinations to ensure that the smoothing does not introduce edge effects.
+        """
+        self.rawimage = image
+        self.image = image.copy()
+        if bbox is not None:
+            x1, y1, x2, y2 = bbox
+            margin = 0  # Minimum for second derivatives
+            if smooth is not None:
+                # Extra margin for low-pass filter
+                margin += int(3 * smooth)
+            if sharp is not None:
+                # Extra margin for high-pass filter
+                margin += int(sharp)
+            assert (
+                (x1 - margin >= 0)
+                and (y1 - margin >= 0)
+                and (x2 + margin <= self.rawimage.shape[1])
+                and (y2 + margin <= self.rawimage.shape[0])
+            ), "Bounding box is too close to the edge of the image"
+            # Full window is used for filtering and derivative calculations
+            self.full_window = (
+                slice(y1 - margin, y2 + margin),
+                slice(x1 - margin, x2 + margin),
+            )
+            self.image = self.image[self.full_window]
+            # The window is what we return
+            self.window = slice(y1, y2), slice(x1, x2)
+            # trim is the window into full_window
+            self.trim = (
+                slice(margin, margin + (y2 - y1)),
+                slice(margin, margin + (x2 - x1)),
+            )
+        else:
+            self.window = slice(None), slice(None)
+            self.trim = slice(None), slice(None)
 
-    # Optionally smooth the image
-    if sigma is not None:
-        kernel = Gaussian2DKernel(sigma)
-        image = convolve_fft(image, kernel)
+        # This all seems far too complicated. Let's at least check
+        # that the two ways of slicing the image are consistent
+        assert np.all(
+            self.image[self.trim] == self.rawimage[self.window]
+        ), "Image window and full window do not match"
 
-    # Calculate the gradient
-    grad_x, grad_y = np.gradient(image)
-    # Calculate the second derivatives
-    grad_xx, grad_xy = np.gradient(grad_x)
-    grad_yx, grad_yy = np.gradient(grad_y)
+        self.smooth = smooth
+        self.sharp = sharp
+        self.max = max
+        self.min = min
+        self._apply_spatial_filters()
+        self._find_curvatures()
 
-    # Calculate the Gaussian and mean curvatures
-    kgauss = grad_xx * grad_yy - grad_xy * grad_yx
-    kmean = (grad_xx + grad_yy) / 2
-    # Calculate the principal curvatures
-    kappa1 = kmean + np.sqrt(kmean**2 - kgauss)
-    kappa2 = kmean - np.sqrt(kmean**2 - kgauss)
+    def _apply_spatial_filters(self):
+        """Apply thresholding and hi-pass and lo-pass filters to the image"""
+        # Threshold the image, replacing with NaN
+        if self.max is not None:
+            self.image[self.image > self.max] = np.nan
+        if self.min is not None:
+            self.image[self.image < self.min] = np.nan
+        # Hi-pass filter to remove smooth features on scales > `sharp`
+        if self.sharp is not None:
+            kernel = Gaussian2DKernel(self.sharp)
+            blurred = convolve_fft(self.image, kernel)
+            self.image = self.image - blurred
+        # Lo-pass filter to remove noise on scales < `smooth`
+        if self.smooth is not None:
+            kernel = Gaussian2DKernel(self.smooth)
+            self.image = convolve_fft(self.image, kernel)
+        # Remove the margin from the image
+        self.image = self.image[self.trim]
 
-    # Calculate the principal directions
-    dir1 = np.arctan2(kappa1 - grad_xx, -grad_xy)
-    dir2 = np.arctan2(kappa2 - grad_xx, -grad_xy)
+    def _find_curvatures(self):
+        """Calculate principal curvatures and principal directions"""
+        # Calculate the gradient
+        self.grad_x, self.grad_y = np.gradient(self.image)
+        # Calculate the second derivatives
+        self.grad_xx, self.grad_xy = np.gradient(self.grad_x)
+        self.grad_yx, self.grad_yy = np.gradient(self.grad_y)
 
-    return kappa1, kappa2, np.rad2deg(dir1), np.rad2deg(dir2)
+        # Calculate the Gaussian and mean curvatures
+        self.kgauss = self.grad_xx * self.grad_yy - self.grad_xy * self.grad_yx
+        self.kmean = (self.grad_xx + self.grad_yy) / 2
+        # Calculate the principal curvatures
+        self.kappa1 = self.kmean + np.sign(self.kmean) * np.sqrt(
+            self.kmean**2 - self.kgauss
+        )
+        self.kappa2 = self.kmean - np.sign(self.kmean) * np.sqrt(
+            self.kmean**2 - self.kgauss
+        )
+
+        # Calculate the principal directions (in degrees cc from x-axis)
+        self.theta1 = np.rad2deg(np.arctan2(self.kappa1 - self.grad_xx, -self.grad_xy))
+        self.theta2 = np.rad2deg(np.arctan2(self.kappa2 - self.grad_xx, -self.grad_xy))
--- test/test_curvature.py
+++ test/test_curvature.py
@@ -1,75 +1,77 @@
 import numpy as np
-from ridge_trace.curvature import principal_curvature
+from ridge_trace.curvature import ImageCurvature
+import pytest
+
+
+@pytest.fixture(scope="module")
+def simple_2d_array():
+    return np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 1, 1, 0],
+            [0, 1, 2, 2, 2, 1, 0],
+            [0, 1, 2, 2, 2, 1, 0],
+            [0, 1, 2, 2, 2, 1, 0],
+            [0, 1, 1, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ],
+        dtype=float,
+    )
 
 
 def test_always_passes():
     assert True
 
 
-def test_principal_curvature():
+def test_principal_curvature(simple_2d_array):
     # Test the principal curvature function
-    # Create a simple 2D array
-    z = np.array(
-        [
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 1, 1, 1, 0],
-            [0, 1, 2, 2, 2, 1, 0],
-            [0, 1, 2, 2, 2, 1, 0],
-            [0, 1, 2, 2, 2, 1, 0],
-            [0, 1, 1, 1, 1, 1, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-        ],
-        dtype=float,
-    )
     # Calculate the principal curvature
-    k1, k2, t1, t2 = principal_curvature(z)
+    ic = ImageCurvature(simple_2d_array)
 
     # np.set_printoptions(precision=2)
     # print(f"{k1=}", f"{k2=}", f"{t1=}", f"{t2=}", sep="\n")
 
     # Check the results
 
-    # The first principal curvature should be the larger of the two
-    assert np.all(k2 <= k1)
+    # The first principal curvature should be the larger magnitude of the two
+    assert np.all(np.abs(ic.kappa2) <= np.abs(ic.kappa1))
 
     # The angle between the two principal directions should be 90 degrees
-    dt = (t1 - t2) % 180
+    dt = (ic.theta1 - ic.theta2) % 180
     # 2024-02-21: The following test was failing initially because
     # some pixels give zero instead of 90 for the angle between the
     # principal directions. This happens when curvature is zero, so I
     # have fixed it with a mask
-    nonzero = (k1 != 0.0) & (k2 != 0.0) & (dt != 0.0)
+    nonzero = (ic.kappa1 != 0.0) & (ic.kappa2 != 0.0) & (dt != 0.0)
     assert np.allclose(dt[nonzero], 90.0, atol=1e-6)
 
 
-def test_principal_curvature_smooth():
-    # Test the principal curvature function
-    # Create a simple 2D array
-    z = np.array(
-        [
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 1, 1, 1, 0],
-            [0, 1, 2, 2, 2, 1, 0],
-            [0, 1, 2, 2, 2, 1, 0],
-            [0, 1, 2, 2, 2, 1, 0],
-            [0, 1, 1, 1, 1, 1, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-        ],
-        dtype=float,
-    )
+def test_principal_curvature_smooth(simple_2d_array):
     # Calculate the principal curvature
-    k1, k2, t1, t2 = principal_curvature(z, sigma=1.0)
+    ic = ImageCurvature(simple_2d_array, smooth=1.0)
 
     np.set_printoptions(precision=2)
-    print(f"{k1=}", f"{k2=}", f"{t1=}", f"{t2=}", sep="\n")
+    # print(f"{ic.kappa1=}", f"{ic.kappa2=}", f"{ic.theta1=}", f"{ic.theta2=}", sep="\n")
 
     # Check the results
 
     # The first principal curvature should be the larger of the two
-    assert np.all(k2 <= k1)
+    assert np.all(np.abs(ic.kappa2) <= np.abs(ic.kappa1))
 
     # The angle between the two principal directions should be 90 degrees
-    dt = (t1 - t2) % 180
-    #
-    nonzero = (k1 != 0.0) & (k2 != 0.0) & (dt != 0.0)
+    dt = (ic.theta1 - ic.theta2) % 180
+    nonzero = (ic.kappa1 != 0.0) & (ic.kappa2 != 0.0) & (dt != 0.0)
     assert np.allclose(dt[nonzero], 90.0, atol=1e-6)
+
+
+def test_principal_curvature_bbox(simple_2d_array):
+    bb_shape = 4, 5
+    bbox = (2, 1, 2 + bb_shape[0], 1 + bb_shape[1])
+    ic = ImageCurvature(simple_2d_array, bbox=bbox)
+
+    np.set_printoptions(precision=2)
+    print(f"{bbox=}")
+    print(f"{ic.image=}")
+    print(f"{ic.rawimage=}")
+    # Compare with reversed bb shape since it is x, y order
+    assert ic.image.shape == bb_shape[::-1]
